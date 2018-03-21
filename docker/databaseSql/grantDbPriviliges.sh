175ec5e6deacf672df479c6d18dcbd23e44a6e93
ejohkie <john.j.kiernan@ericsson.com>
2018-03-21 12:28:23 +0000

    Mysql grant privileges to all ipaddresses

--- docker/databaseSql/grantDbPriviliges.sh
+++ docker/databaseSql/grantDbPriviliges.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -u root -proot -e "GRANT ALL ON *.* to root@'172.21.0.1' IDENTIFIED BY 'root'"
+mysql -u root -proot -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'root'"
 mysql -u root -proot -e "FLUSH PRIVILEGES"
